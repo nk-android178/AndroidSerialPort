07795b732e1d6ddcd42bdd210ec707b473f4a427
Administrator <1789488626@qq.com>
2020-10-13 09:46:34 +0800

    添加新的小例子

--- app/src/main/java/com/nk/androidserialport/MainActivity.java
+++ app/src/main/java/com/nk/androidserialport/MainActivity.java
@@ -11,7 +11,6 @@ import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
-
 public class MainActivity extends Activity {
     private String TAG = "MainActivity";
     private Button button;
